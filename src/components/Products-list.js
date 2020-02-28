742a4294b7b77bd1ac9eeee3c105a4adefd8a173
elenamlopez <elena.mateos1@gmail.com>
2020-02-28 10:23:33 +0100

    Add styles to Home page

--- src/components/Products-list.js
+++ src/components/Products-list.js
@@ -17,25 +17,30 @@ export class ProductsList extends Component {
    return this.props.onClick
   }
 
-   _renderProducts = () =>{
-    
+  _renderProducts = () =>{
     return (this.state.productsList.map((product) => {
       return (
         <a 
+          className='product-list--link'
           key={product.id} 
           href={`?id=${product.name.replace(/ /g, "")}`}
           onClick={this.onClick}
           >
           <li className='product-list--item'>
-            <p>Name: {product.name}</p>
-            <img 
-              alt={product.name} 
-              src={`../../images/${product.imageFileName}`}
-              className='product-list--image'
-              />
-            <p>Brand: {product.manufacturer}</p>
-            <p>Price: {product.price}</p>
-            <p>Processor: {product.processor}. Ram: {product.ram}. Screen: {product.screen}</p>
+            <div className='product-list--item-head'>
+              <h3 className='product-list--item-title'>{product.name}</h3>
+              <img 
+                alt={product.name} 
+                src={`../../images/${product.imageFileName}`}
+                className='product-list--image'
+                />
+            </div>
+            <div className='product-list-item--info-display'>
+              <p className='product-list-item--info'>Price: {product.price} | Brand: {product.manufacturer}</p>
+              <p className='product-list-item--info'>Processor: {product.processor}. </p>
+              <p>Ram: {product.ram}. </p>
+              <p>Screen: {product.screen}</p>
+            </div>
           </li> 
         </a>
       )
